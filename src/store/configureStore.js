38a0520dd01822cfc12cb0a62d28a5540519864f
unknown <asifrizwan6989@gmail.com>
2020-06-16 17:00:29 +0530

    Json problem fixed

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,16 +2,11 @@ import {createStore, applyMiddleware} from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import reduxThunk from 'redux-thunk';
 import rootReducer from '../reducers';
-import listedproduct from '../assets/products.json';
 
-// const initialState = {
-//   productList: jsonProductList
-// };
+
 
 export default function configureStore() {
-  console.log("OOOooo~!@#$%^"+JSON.stringify(listedproduct) );
- // console.log('initialstate',initialState);
-    return createStore(rootReducer,initialState, applyMiddleware(reduxThunk, reduxImmutableStateInvariant())
+    return createStore(rootReducer, applyMiddleware(reduxThunk, reduxImmutableStateInvariant())
   );
 } 
 
